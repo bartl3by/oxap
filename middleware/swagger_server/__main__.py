8d02fd365adadec7ff83f0191e6cb3bb86d7ed97
Benjamin Otterbach <bartl3by@t-online.de>
2018-01-15 10:38:17 -0800

    PEP-8

--- middleware/swagger_server/__main__.py
+++ middleware/swagger_server/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import connexion
-from .encoder import JSONEncoder
-from .configuration import define_configuration_options
 import tornado
 from tornado.options import define, options
-import os
-from tornado.log import enable_pretty_logging
-import logging
 
+from .configuration import define_configuration_options
+from .encoder import JSONEncoder
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/', swagger_json=True, swagger_ui=True)
@@ -18,5 +15,7 @@ if __name__ == '__main__':
     tornado.options.parse_command_line()
     tornado.options.parse_config_file(options.config_file)
 
-    app.add_api('swagger.yaml', arguments={'title': 'This is the Open-Xchange Admin Panel API. This API is the connector to the middleware that handles provisioning tasks towards Open-Xchange products such as Dovecot or App Suite.'})
+    app.add_api('swagger.yaml', arguments={'title': 'This is the Open-Xchange Admin Panel API. This API is the '
+                                                    'connector to the middleware that handles provisioning tasks '
+                                                    'towards Open-Xchange products such as Dovecot or App Suite.'})
     app.run(server='tornado', port=options.server_port)
